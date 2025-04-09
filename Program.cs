26645d2fd85404306a4b9967bca403cbe095d6bd
orrsanFran <orr.fran@oulook.com>
2025-04-09 11:48:00 -0400

    Initial commit - project bootstrapped

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PSF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
